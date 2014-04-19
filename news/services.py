9d69e0110860b09349280956f57f0c4f40ca4869
bongster <bongster88@gmail.con>
2014-04-19 22:56:14 +0900

    setting

--- news/services.py
+++ news/services.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import urllib2
+import urllib
 from lxml import etree as ET
 import collections
 import re
@@ -14,11 +18,14 @@ def newsInfo():
 	infos = []
 	url = 'http://tab.search.daum.net/dsa/search?w=news&m=rss&SortType=1&q=%B9%DA%BF%F8%BC%F8&cp=37730750'
 	rss = Rss(url,["37736525","70640889,264","1119","120","78305928"])
+	data = {}
+	data['q'] = '박원순'
+	print data['q']
+	data['cp'] = '37730750'
+#	req = urllib2.Request(url,data)
 	response = urllib2.urlopen(url)
 	xml =  response.read().decode('cp949', 'ignore').encode('utf-8')
-#	xml =  response.read()
 	parser = ET.XMLParser(encoding="utf-8")
-#	parser = ET.XMLParser()
 	rss =  ET.fromstring(xml,parser = parser)
 
 	items = rss.findall('.//channel/item')
@@ -42,9 +49,7 @@ def child(url):
 	brace_re = '[\[\{\(].*?[\]\}\)]|[\']';
 	email_re = '[^@]+@[^@]+\.[^@]+' 
 	default_news = ET.tostring(newsbody,method="text",encoding="utf-8")
-#	default_news = ET.tostring(newsbody,method="text")
 	no_brace_news = re.sub(brace_re,'',default_news)
-	print no_brace_news
 	return no_brace_news
 
 def wordCount(words,splitword = ' '):
