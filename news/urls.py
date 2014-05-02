862ec948386650ea0e8da6d70997384db6e88d9a
bongsungju <bongster88@gmail.com>
2014-05-02 13:41:26 +0900

    setting schedule and memcache

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,6 @@ from news import views
 urlpatterns = patterns('',
 		url(r'^$', views.index, name='index'),
 		url(r'^info$', views.info, name='info'),
+		url(r'^update$', views.update, name='update'),
 )
 
--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from news.models import Greeting
-from google.appengine.api import users
+from google.appengine.api import users, memcache
 from django.http import HttpResponse
 from services import newsInfo
 import json
@@ -12,6 +12,31 @@ def index(request):
 	return render(request, 'index.html',context)
 
 def info(request):
-	response_data = {}
-	response_data['news'] =  newsInfo()
+	info = memcache.get('news')
+	if info is not None:
+		response_data = {}
+		response_data['news'] = info
+		return HttpResponse(json.dumps(response_data),content_type="application/json")
+	else:
+		response_data = {}
+		news = newsInfo()
+		response_data['news'] = news
+		memcache.add('news',news)
+		return HttpResponse(json.dumps(response_data),content_type="application/json")
+
+def update(request):
+	info = memcache.get('news')
+	news = newsInfo()
+	response_data= {}
+	if info is not None:
+		response_data['status']= 'update'
+		memcache.replace('news',news)
+	else:
+		response_data['status']= 'create'
+		memcache.add('news',news)
+
 	return HttpResponse(json.dumps(response_data),content_type="application/json")
+	
+	
+
+
